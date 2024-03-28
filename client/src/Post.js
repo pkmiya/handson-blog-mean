80f8dbb97d55583773d4c23bd0e23ff4cced610a
pkmiya <xbzd.cnlv@icloud.com>
2024-03-28 22:48:52 +0900

    frontend: add Post component

--- client/src/Post.js
+++ client/src/Post.js
@@ -1,16 +1,22 @@
-export default function Post() {
+import { format } from 'date-fns';
+import { Link } from 'react-router-dom';
+
+export default function Post({ post }) {
+    const { _id, title, summary, author, cover, createdAt } = post;
+    console.log(post);
     return (
         <div className="post">
             <div className="image">
-                <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1" alt="" />
+                <Link to={`/post/${_id}`}><img src={"http://localhost:4000/" + cover} alt="" /></Link>
+
             </div>
             <div className="texts">
-                <h2>Full-house battery backup coming later this year</h2>
+                <Link to={`/post/${_id}`}><h2>{title}</h2></Link>
                 <p className="info">
-                    <a className="author">Yusaku Miyata</a>
-                    <time>2023-02-23 15:13</time>
+                    <a className="author">{author.username ?? ''}</a>
+                    <time>{format(new Date(createdAt), 'MMM d, yyyy HH:mm')}</time>
                 </p>
-                <p className="summary">Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
+                <p className="summary">{summary}</p>
             </div>
         </div>
     )
