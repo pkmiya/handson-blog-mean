8d5f567244c6c3316a13249425455facc8758998
pkmiya <xbzd.cnlv@icloud.com>
2024-03-28 22:49:32 +0900

    frontend: add pages

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import { UserContextProvider } from './UserContext';
 import CreatePost from './pages/CreatePost';
+import Postpage from './pages/Postpage';
+import EditPost from './pages/EditPost';
 
 function App() {
 	return (
@@ -23,6 +25,12 @@ function App() {
 					} />
 					<Route path="/create" element={
 						<CreatePost />
+					} />
+					<Route path="/post/:id" element={
+						<Postpage />
+					} />
+					<Route path="/edit/:id" element={
+						<EditPost />
 					} />
 				</Route>
 			</Routes>
